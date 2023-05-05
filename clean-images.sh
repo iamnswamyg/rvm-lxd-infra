a0bf1132d8eec327faf6302be9484487daa46e2c
iamnswamyg <106902046+iamnswamyg@users.noreply.github.com>
2023-05-05 10:51:14 +0200

    Update clean-images.sh

--- clean-images.sh
+++ clean-images.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-SCRIPT_PREFIX="salt"
-SALT_MINION_NAME=${SCRIPT_PREFIX}"-minion"
-SALT_MASTER_NAME=${SCRIPT_PREFIX}"-master"
+SCRIPT_PREFIX="rvm"
 
-image_names=("${SALT_MASTER_NAME}" "${SALT_MINION_NAME}")
+image_names=("${SCRIPT_PREFIX}")
 
 # Loop through the list of items
 for image_name in "${image_names[@]}"
@@ -15,10 +13,6 @@ do
     fi
 done
 
-image_fingerprint="39da8bdecb9450521ec97683265fbc51fa1f29c0eabae102e7be78e787788047"
-if lxc image list --format=json | jq -r '.[] | .fingerprint' | grep -q "$image_fingerprint"; then
-        echo "Deleting Image $image_fingerprint is locally"
-        sudo lxc image delete $image_fingerprint
-fi
+
 echo "listing the images"
 lxc image list --format=json | jq -r '.[] | .aliases' | jq -r '.[].name' 
